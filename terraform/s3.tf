c1a23c3601490fad6c7c98dd679ef157b6ab4134
Priyanshu <pshekhar830@gmail.com>
2023-10-31 18:38:57 +0530

    completed s3 code

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -7,70 +7,44 @@ resource "aws_s3_bucket" "s3_bucket" {
   }
 }
 
-# # Bucket private access
-# resource "aws_s3_bucket_acl" "s3_bucket_acl" {
-#   bucket = aws_s3_bucket.s3_bucket.id
-#   acl    = "private"
-# }
+# Bucket ownership controls
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_ownership_controls" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
 
-# # Enable bucket versioning
-# resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
-#   bucket = aws_s3_bucket.s3_bucket.id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
+  depends_on = [
+    aws_s3_bucket.s3_bucket
+  ]
+}
 
-# # Enable default Server Side Encryption
-# resource "aws_s3_bucket_server_side_encryption_configuration" "s3_bucket_server_side_encryption" {
-#   bucket = aws_s3_bucket.s3_bucket.id
+# Bucket private access
+resource "aws_s3_bucket_acl" "s3_bucket_acl" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  acl    = "private"
 
-#   rule {
-#     apply_server_side_encryption_by_default {
-#         kms_master_key_id = aws_kms_key.kms_s3_key.arn
-#         sse_algorithm     = "aws:kms"
-#     }
-#   }
-# }
+  depends_on = [
+    aws_s3_bucket.s3_bucket,
+    aws_s3_bucket_ownership_controls.s3_bucket_ownership_controls
+  ]
+}
 
-# # Creating Lifecycle Rule
-# resource "aws_s3_bucket_lifecycle_configuration" "s3_bucket_lifecycle_rule" {
-#   # Must have bucket versioning enabled first
-#   depends_on = [aws_s3_bucket_versioning.s3_bucket_versioning]
+# Bucket versioning
+resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
 
-#   bucket = aws_s3_bucket.s3_bucket.id
+# Bucket public access
+resource "aws_s3_bucket_public_access_block" "s3_bucket_access" {
+  bucket = aws_s3_bucket.s3_bucket.id
 
-#   rule {
-#     id = "basic_config"
-#     status = "Enabled"
-
-#     filter {
-#       prefix = "config/"
-#     }
-
-#     noncurrent_version_transition {
-#       noncurrent_days = 30
-#       storage_class   = "STANDARD_IA"
-#     }
-
-#     noncurrent_version_transition {
-#       noncurrent_days = 60
-#       storage_class   = "GLACIER"
-#     }
-    
-#     noncurrent_version_expiration {
-#       noncurrent_days = 90
-#     }
-#   }
-# }
-
-# # Disabling bucket public access
-# resource "aws_s3_bucket_public_access_block" "s3_bucket_access" {
-#   bucket = aws_s3_bucket.s3_bucket.id
-
-#   # Block public access
-#   block_public_acls   = true
-#   block_public_policy = true
-#   ignore_public_acls = true
-#   restrict_public_buckets = true
-# }
+  # Block public access
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
