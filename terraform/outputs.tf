944420b45a8be95cadd86e48419b8f2ab11b75ec
Priyanshu <pshekhar830@gmail.com>
2023-11-01 00:27:53 +0530

    added outputs for iam role and policy

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
 # S3 ############################################
 
 data "aws_s3_bucket" "s3_bucket" {
-  bucket = "s3-bucket-psshri-8441"
+    bucket = "s3-bucket-psshri-8441"
 }
 
 output "s3_arn" {
@@ -10,4 +10,16 @@ output "s3_arn" {
 
 output "s3_bucket_name" {
     value = aws_s3_bucket.s3_bucket.bucket
+}
+
+# IAM Role ######################################
+
+output "iam_role_arn" {
+    value = aws_iam_role.lambda_role.arn
+}
+
+# IAM Policy ####################################
+
+output "iam_policy_arn" {
+    value = aws_iam_policy.s3_policy.arn
 }
