99fc6eb5f983c79ec827aac70494eb78b5a54ee6
Priyanshu <pshekhar830@gmail.com>
2023-11-01 22:36:08 +0530

    added resource block for lambda integration with apigw

--- terraform/api_gw.tf
+++ terraform/api_gw.tf
@@ -2,3 +2,25 @@ resource "aws_apigatewayv2_api" "http_api" {
   name          = "http_api"
   protocol_type = "HTTP"
 }
+
+# resource "aws_apigatewayv2_deployment" "http_api_deployment" {
+#   api_id      = aws_apigatewayv2_api.http_api.id
+
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+
+#   depends_on = [
+#     aws_apigatewayv2_api.http_api
+#   ]
+# }
+
+# Lambda Integration
+resource "aws_apigatewayv2_integration" "http_api_integration" {
+  api_id           = aws_apigatewayv2_api.http_api.id
+  integration_type = "AWS_PROXY"
+
+  connection_type    = "INTERNET"
+  integration_method = "POST"
+  integration_uri    = aws_lambda_function.lambda_function.invoke_arn
+}
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -67,11 +67,11 @@ data "archive_file" "python_code_zip" {
 
 # Lambda Function
 resource "aws_lambda_function" "lambda_function" {
-  filename         = "${path.module}/python/hello-python.zip"
-  function_name    = "test_lambda_function"
-  role             = aws_iam_role.lambda_role.arn
-  handler          = "index.lambda_handler"
-  runtime          = "python3.11"
+  filename      = "${path.module}/python/hello-python.zip"
+  function_name = "test_lambda_function"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.lambda_handler"
+  runtime       = "python3.11"
   # source_code_hash = data.archive_file.python_code_zip.output_base64sha256
 
   depends_on = [
