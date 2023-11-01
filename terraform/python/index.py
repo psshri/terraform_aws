38db221acf653cebfdd524b5b5360a336b26dd1d
Priyanshu <pshekhar830@gmail.com>
2023-11-02 01:16:25 +0530

    code sends data to s3 now

--- terraform/python/index.py
+++ terraform/python/index.py
@@ -12,21 +12,27 @@ s3_client = boto3.client('s3')
 def lambda_handler(event, context):
     # print(S3_BUCKET_NAME)
     # print(S3_KEY_PREFIX)
-    s3_object_key = S3_KEY_PREFIX + str(int(time.time())) + '.json'
-    print(s3_object_key)
-    
+    # s3_object_key = S3_KEY_PREFIX + str(int(time.time())) + '.json'
+    # print(s3_object_key)
+
     try:
         json_data = json.loads(event['body'])
 
         s3_object_key = S3_KEY_PREFIX + str(int(time.time())) + '.json'
 
-        print(s3_object_key)
+        s3_client.put_object(
+            Bucket=S3_BUCKET_NAME,
+            Key=s3_object_key,
+            Body=json.dumps(json_data)
+        )
+
+        # print(s3_object_key)
 
 
-        print("Recieved JSON data: ", json_data)
+        # print("Recieved JSON data: ", json_data)
         return {
             'statusCode': 200,
-            'body': json.dumps('Data received and printed successfully')
+            'body': json.dumps('Request JSON Body recieved and pushed to s3 successfully')
         }
 
     except Exception as e:  
