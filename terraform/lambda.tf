e67e1181caba64a95c4b53c8e1b67ecc4a647ac5
Priyanshu <pshekhar830@gmail.com>
2023-11-01 14:32:02 +0530

    added outputs for lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,7 +1,7 @@
 # IAM Role ######################################
 resource "aws_iam_role" "lambda_role" {
-    name = "lambda_role"
-    assume_role_policy = <<EOF
+  name               = "lambda_role"
+  assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -19,9 +19,9 @@ EOF
 
 # IAM Policy ####################################
 resource "aws_iam_policy" "s3_policy" {
-    name        = "s3_policy"
-    description = "s3_get_put_policy"
-    policy = <<EOF
+  name        = "s3_policy"
+  description = "s3_get_put_policy"
+  policy      = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
@@ -37,26 +37,63 @@ resource "aws_iam_policy" "s3_policy" {
 }
 EOF
 
-    depends_on = [
-        aws_s3_bucket.s3_bucket
-    ]
+  depends_on = [
+    aws_s3_bucket.s3_bucket
+  ]
 
 }
 
 # IAM Role Policy Attachment
 resource "aws_iam_role_policy_attachment" "lambda_s3_role_policy_attachment" {
-    role = "${aws_iam_role.lambda_role.id}"
-    policy_arn = "${aws_iam_policy.s3_policy.arn}"
+  role       = aws_iam_role.lambda_role.id
+  policy_arn = aws_iam_policy.s3_policy.arn
 
-    depends_on = [
-        aws_iam_role.lambda_role,
-        aws_iam_policy.s3_policy
-    ]
+  depends_on = [
+    aws_iam_role.lambda_role,
+    aws_iam_policy.s3_policy
+  ]
 }
 
 # Zip the file
 data "archive_file" "python_code_zip" {
-type        = "zip"
-source_dir  = "${path.module}/python/"
-output_path = "${path.module}/python/hello-python.zip"
+  type        = "zip"
+  source_file = "${path.module}/python/index.py"
+  output_path = "${path.module}/python/hello-python.zip"
 }
+
+resource "aws_lambda_function" "lambda_function" {
+  filename         = "${path.module}/python/hello-python.zip"
+  function_name    = "test_lambda_function"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "index.lambda_handler"
+  runtime          = "python3.11"
+  source_code_hash = data.archive_file.python_code_zip.output_base64sha256
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_s3_role_policy_attachment,
+  ]
+}
+
+# resource "aws_lambda_function" "s3" {
+#   function_name = "s3"
+#   s3_bucket = aws_s3_bucket.lambda_bucket.id
+#   s3_key    = aws_s3_object.lambda_s3.key
+#   runtime = "nodejs16.x"
+#   handler = "function.handler"
+#   source_code_hash = data.archive_file.lambda_s3.output_base64sha256
+#   role = aws_iam_role.s3_lambda_exec.arn
+# }
+
+# resource "aws_lambda_function" "test_lambda" {
+#   filename      = "lambda_function_payload.zip"
+#   function_name = "lambda_function_name"
+#   role          = aws_iam_role.iam_for_lambda.arn
+#   handler       = "index.test"
+#   source_code_hash = data.archive_file.lambda.output_base64sha256
+#   runtime = "nodejs18.x"
+#   environment {
+#     variables = {
+#       foo = "bar"
+#     }
+#   }
+# }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,29 +1,43 @@
 # S3 ############################################
 
 data "aws_s3_bucket" "s3_bucket" {
-    bucket = "s3-bucket-psshri-8441"
+  bucket = "s3-bucket-psshri-8441"
 }
 
 output "s3_arn" {
-    value = aws_s3_bucket.s3_bucket.arn
+  value = aws_s3_bucket.s3_bucket.arn
 }
 
 output "s3_bucket_name" {
-    value = aws_s3_bucket.s3_bucket.bucket
+  value = aws_s3_bucket.s3_bucket.bucket
 }
 
 # IAM Role ######################################
 
 output "iam_role_arn" {
-    value = aws_iam_role.lambda_role.arn
+  value = aws_iam_role.lambda_role.arn
 }
 
 output "iam_role_id" {
-    value = aws_iam_role.lambda_role.id
+  value = aws_iam_role.lambda_role.id
 }
 
 # IAM Policy ####################################
 
 output "iam_policy_arn" {
-    value = aws_iam_policy.s3_policy.arn
+  value = aws_iam_policy.s3_policy.arn
+}
+
+# Lambda ########################################
+
+output "lambda_arn" {
+  value = aws_lambda_function.lambda_function.arn
+}
+
+output "lambda_id" {
+  value = aws_lambda_function.lambda_function.id
+}
+
+output "lambda_invoke_arn" {
+  value = aws_lambda_function.lambda_function.invoke_arn
 }
