5884d3d070b699e67151f1968fb0ef5bfb6840b5
Priyanshu <pshekhar830@gmail.com>
2023-11-02 00:59:47 +0530

    generated unique s3_object_key based on time

--- terraform/python/index.py
+++ terraform/python/index.py
@@ -1,15 +1,28 @@
 import json
 import os
+import boto3
+import time
 
 # Environment Variables
 S3_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
 S3_KEY_PREFIX = 'json_body'
 
+s3_client = boto3.client('s3')
+
 def lambda_handler(event, context):
-    print(S3_BUCKET_NAME)
-    print(S3_KEY_PREFIX)
+    # print(S3_BUCKET_NAME)
+    # print(S3_KEY_PREFIX)
+    s3_object_key = S3_KEY_PREFIX + str(int(time.time())) + '.json'
+    print(s3_object_key)
+    
     try:
         json_data = json.loads(event['body'])
+
+        s3_object_key = S3_KEY_PREFIX + str(int(time.time())) + '.json'
+
+        print(s3_object_key)
+
+
         print("Recieved JSON data: ", json_data)
         return {
             'statusCode': 200,
