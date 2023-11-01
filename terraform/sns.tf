149fd494d7db3a2a3e2d6766876bd2f68a1763dc
Priyanshu <pshekhar830@gmail.com>
2023-11-01 15:10:38 +0530

    added depends on in sns topic subs

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -1,9 +1,15 @@
+# SNS Topic #####################################
 resource "aws_sns_topic" "sns_topic" {
   name = "lambda_sns_topic"
 }
 
+# SNS Topic Subscription ########################
 resource "aws_sns_topic_subscription" "sns_topic_subscription" {
   topic_arn = aws_sns_topic.sns_topic.arn
   protocol  = "email"
   endpoint  = "psshri@outlook.com"
+
+  depends_on = [
+    aws_sns_topic.sns_topic
+  ]
 }
